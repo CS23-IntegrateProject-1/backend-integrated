383bccd5c9ce06bba43264481cbef82995d5bf32
Yunthecloud <sulei.yin1@kmutt.ac.th>
2023-10-22 03:54:26 +0700

    [ADD] routes for feature7

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,6 @@ const port = process.env.PORT || 3000;
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello, Express with TypeScript!");
 });
-
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
--- src/routes/feature7.routes.ts
+++ src/routes/feature7.routes.ts
@@ -2,12 +2,32 @@ import { Router } from "express";
 
 // here import your controllers(function)
 import {
+    getMenuById,
+    getMenus,
     getfeature7,
+    getVenueById,
+    getCustomerById,
+    addProductToOrderDetails,
+    createNewOrder,
+    createOrderFromOrderDetails,
+    menuOrderCount,
+    getOrdersByCustomerIdandDate,
+    getReceipt
 } from "../controllers/feature7.controller";
 
 const feature7Router = Router();
 
 // here define your routes
 feature7Router.get("/", getfeature7);
-
+//get all menus
+feature7Router.get("/getMenus",getMenus);
+feature7Router.get("/getMenuById",getMenuById);
+feature7Router.get("/getVenueById",getVenueById);
+feature7Router.get("/getCustomerById",getCustomerById);
+feature7Router.post("/addProductToOrderDetails",addProductToOrderDetails);
+feature7Router.post("/createNewOrder",createNewOrder);
+feature7Router.post("/createOrderFromOrderDetails",createOrderFromOrderDetails);
+feature7Router.get("/menuOrderCount",menuOrderCount);
+feature7Router.get("/getOrdersByCustomerIdandDate",getOrdersByCustomerIdandDate);
+feature7Router.get("/getReceipt",getReceipt);
 export default feature7Router;
