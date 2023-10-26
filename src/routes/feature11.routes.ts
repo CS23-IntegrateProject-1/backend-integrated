5821cbb98639a1fa99001a3c9f3b68bdc5314543
Paramita Saenghao <paramita.saen@mail.kmutt.ac.th>
2023-10-26 20:12:23 +0700

    [EDIT] create post endpoint
    - create endpoint for adding article

--- src/routes/feature11.routes.ts
+++ src/routes/feature11.routes.ts
@@ -1,4 +1,5 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
 // here import your controllers(function)
 import {
@@ -6,8 +7,44 @@ import {
 } from "../controllers/feature11.controller";
 
 const feature11Router = Router();
+const prisma = new PrismaClient();
 
 // here define your routes
 feature11Router.get("/", getfeature11);
+
+//const getAllUsers = async (req: Request, res: Response) => {
+//  try {
+//    const allUsers = await prisma.venue.findMany();
+
+//    res.json(allUsers);
+//  } catch (error) {
+//    console.error("Error retrieving all users:", error);
+//    res.status(500).json({ error: "Internal server error" });
+//  }
+//};
+
+const addArticle = async (req: Request, res: Response) => {
+  try {
+    const { topic, content, category, venueId, userId } = req.body;
+
+    const newArticle = await prisma.article.create({
+      data: {
+        topic,
+        content,
+        category,
+        venueId,
+        userId,
+      },
+    });
+
+    res.json(newArticle);
+  } catch (error) {
+    console.error(error);
+    res.json({ error: "Internal server error" });
+  }
+  };
+
+feature11Router.post("/addarticle", addArticle);
+//feature11Router.get("/getAllUsers", getAllUsers);
 
 export default feature11Router;
