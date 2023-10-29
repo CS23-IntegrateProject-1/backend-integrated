065d313c6010cc46252adfa1e009ccabc4754bd6
Dylan Mac Yves <macyvesdylan@gmail.com>
2023-10-29 23:56:18 +0700

    added get methods

--- src/controllers/feature3.controller.ts
+++ src/controllers/feature3.controller.ts
@@ -3,10 +3,7 @@ import { Response, Request } from "express";
 
 const feature3Client = new PrismaClient();
 
-export const getfeature3 = async (req: Request, res: Response) => {
-    
-};
-
+export const getfeature3 = async (req: Request, res: Response) => {};
 
 // example of controller getAllAuthors
 // export const getAllAuthors = async (req: Request, res: Response) => {
@@ -16,3 +13,83 @@ export const getfeature3 = async (req: Request, res: Response) => {
 //     console.log(e);
 //   }
 // };
+
+export const getAdvertisements = async (req: Request, res: Response) => {
+    try {
+        const advertisement = await feature3Client.advertisement.findMany();
+        return res.json(advertisement);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(error);
+    }
+};
+
+export const getVenues = async (req: Request, res: Response) => {
+    try {
+        const venue = await feature3Client.venue.findMany();
+        return res.json(venue);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(error);
+    }
+};
+
+export const getVenueReviews = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const reviews = await feature3Client.venue_reviews.findMany({
+            where: {
+                venuId: parseInt(id),
+            },
+        });
+        return res.json(reviews);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json(error);
+    }
+};
+
+export const getVenueOverviews = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const overviews = await feature3Client.venue_overviews.findMany({
+            where: {
+                venueId: parseInt(id),
+            },
+        });
+        return res.json(overviews);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json(error);
+    }
+};
+
+export const getVenuePhotos = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const photos = await feature3Client.venue_photo.findMany({
+            where: {
+                venueId: parseInt(id),
+            },
+        });
+        return res.json(photos);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json(error);
+    }
+};
+
+export const getSearchHistory = async (req: Request, res: Response) => {
+    const { user_id } = req.params;
+    try {
+        const history = await feature3Client.search_history.findMany({
+            where: {
+                id: parseInt(user_id),
+            },
+        });
+        return res.json(history);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json(error);
+    }
+};
--- src/routes/feature3.routes.ts
+++ src/routes/feature3.routes.ts
@@ -2,12 +2,23 @@ import { Router } from "express";
 
 // here import your controllers(function)
 import {
-    getfeature3,
+    getVenues,
+    getVenueReviews,
+    getVenueOverviews,
+    getVenuePhotos,
+    getSearchHistory,
+    getAdvertisements,
 } from "../controllers/feature3.controller";
 
 const feature3Router = Router();
 
 // here define your routes
-feature3Router.get("/", getfeature3);
+feature3Router.get("/", getVenues);
+feature3Router.get("/reviews/:id", getVenueReviews);
+feature3Router.get("/overviews/:id", getVenueOverviews);
+feature3Router.get("/photos/:id", getVenuePhotos);
+feature3Router.get("/advertisement", getAdvertisements);
+
+feature3Router.get("/history/:user_id", getSearchHistory);
 
 export default feature3Router;
