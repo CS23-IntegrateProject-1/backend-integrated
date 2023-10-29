e46be4cdce19671470e713e6ab7d4daed1f57774
Vincent-NHtun <naylinhtunclifton1466@gmail.com>
2023-10-29 17:43:30 +0700

    [EDIT] Feature12 Add Create and Read

--- src/routes/feature12.routes.ts
+++ src/routes/feature12.routes.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 
 // here import your controllers(function)
 import {
-    getfeature12,
+  createGQuestions,
+  getAllGeneralQuestions,
 } from "../controllers/feature12.controller";
 
 const feature12Router = Router();
 
 // here define your routes
-feature12Router.get("/", getfeature12);
+feature12Router.post("/", createGQuestions);
+feature12Router.get("/", getAllGeneralQuestions);
+
 
 export default feature12Router;
