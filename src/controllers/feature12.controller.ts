f68d6cd8e88e333f888ed99c9ec935a3a2fe0a40
Andhika_RK <andhikarestu873@gmail.com>
2023-10-29 18:02:41 +0700

    [EDIT] fix bug on feature12 controller

--- src/controllers/feature12.controller.ts
+++ src/controllers/feature12.controller.ts
@@ -1,18 +1,33 @@
 import { PrismaClient } from "@prisma/client";
 import { Response, Request } from "express";
-
 const feature12Client = new PrismaClient();
-
 export const getfeature12 = async (req: Request, res: Response) => {
-    
+
+};
+
+//Create
+export const createGQuestions = async (req: Request, res: Response) => {
+  const { Questionid, QuestionText } = req.body;
+  try {
+    const GQuestions = await feature12Client.general_question.create({
+      data: {
+        id: Questionid,
+        g_question: QuestionText,
+      },
+    });
+    return res.status(201).json(GQuestions);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 };
 
 
-// example of controller getAllAuthors
-// export const getAllAuthors = async (req: Request, res: Response) => {
-//   try {
-//     const allAuthors = await feature1Client.modelName.findMany({
-//   } catch (e) {
-//     console.log(e);
-//   }
-// };
+//Read
+export const getAllGeneralQuestions = async (req: Request, res: Response) => {
+  try {
+    const GQuestions = await feature12Client.general_question.findMany();
+    return res.status(200).json(GQuestions);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
--- src/routes/feature12.routes.ts
+++ src/routes/feature12.routes.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 
 // here import your controllers(function)
 import {
-    getfeature12,
-} from "../controllers/feature12.controller";
-
-const feature12Router = Router();
-
-// here define your routes
-feature12Router.get("/", getfeature12);
-
-export default feature12Router;
+    createGQuestions,
+    getAllGeneralQuestions,
+  } from "../controllers/feature12.controller";
+  
+  const feature12Router = Router();
+  
+  // here define your routes
+  feature12Router.post("/", createGQuestions);
+  feature12Router.get("/", getAllGeneralQuestions);
+  
+  
+  export default feature12Router;
