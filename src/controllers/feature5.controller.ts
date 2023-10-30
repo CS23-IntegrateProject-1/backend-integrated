17e080902a18416ec97fc760aa6d5fbea4ec4901
Dxchmxww <120299511+Dxchmxww@users.noreply.github.com>
2023-10-30 16:47:08 +0700

    [ADD] getAdvertisement
    [ADD] getAdvertisement

--- src/controllers/feature5.controller.ts
+++ src/controllers/feature5.controller.ts
@@ -3,11 +3,18 @@ import { Response, Request } from "express";
 
 const feature5Client = new PrismaClient();
 
-export const getfeature5 = async (req: Request, res: Response) => {
-    
+export const getfeature5 = async (req: Request, res: Response) => {};
+
+export const getAdvertisement = async (req: Request, res: Response) => {
+    try {
+            const allAdvertisements = await feature5Client.advertisement.findMany();
+            res.status(200).json(allAdvertisements);
+            
+    } catch (e) {
+            console.log(e);
+            res.status(500).json({ error: 'Failed to retrieve data' });
+          }
 };
-
-
 // example of controller getAllAuthors
 // export const getAllAuthors = async (req: Request, res: Response) => {
 //   try {
--- src/routes/feature5.routes.ts
+++ src/routes/feature5.routes.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 // here import your controllers(function)
 import {
     getfeature5,
+    getAdvertisement
 } from "../controllers/feature5.controller";
 
 const feature5Router = Router();
 
 // here define your routes
 feature5Router.get("/", getfeature5);
+feature5Router.get("/Advertisement", getAdvertisement);
 
 export default feature5Router;
