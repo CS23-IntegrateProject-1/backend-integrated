fe280f3d8d284d4ede411e2f9322313ee1f71387
Vincent-NHtun <naylinhtunclifton1466@gmail.com>
2023-11-04 22:48:32 +0700

    [EDIT] Add some routes for displaying QnA

--- src/controllers/feature12.controller.ts
+++ src/controllers/feature12.controller.ts
@@ -5,17 +5,51 @@ export const getfeature12 = async (req: Request, res: Response) => {
 
 };
 
-//Create
-export const createGQuestions = async (req: Request, res: Response) => {
-  const { Questionid, QuestionText } = req.body;
+//Display All the question from General Question table (just the question column)
+export const displayAllGQuestions = async (req: Request, res: Response) => {
   try {
-    const GQuestions = await feature12Client.general_questions.create({
-      data: {
-        id: Questionid,
-        g_question: QuestionText,
+    const questions = await feature12Client.general_questions.findMany({
+      select: {
+        g_question: true,
       },
     });
-    return res.status(201).json(GQuestions);
+    res.json(questions);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+
+//Display Answer corresponding to the question
+export const displayAnswer = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const answers = await feature12Client.general_answers.findMany({
+      where: {
+        gQuestionId: parseInt(id),
+      },
+      select: {
+        g_answer: true,
+      },
+    });
+
+    res.json(answers);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+
+//Create
+export const createGAnswer = async (req: Request, res: Response) => {
+  const { id, gQuestionId, g_answer } = req.body;
+  try {
+    const GAnswer = await feature12Client.general_answers.create({
+      data: {
+        id: id,
+        gQuestionId: gQuestionId,
+        g_answer: g_answer
+      },
+    });
+    return res.status(201).json(GAnswer);
   } catch (error) {
     return res.status(500).json({ error });
   }
@@ -25,11 +59,47 @@ export const createGQuestions = async (req: Request, res: Response) => {
 //Read
 export const getAllGeneralQuestions = async (req: Request, res: Response) => {
   try {
-    const GQuestions = await feature12Client.general_questions.findMany();
-    return res.status(200).json(GQuestions);
+    const GAnswer = await feature12Client.general_answers.findMany();
+    return res.status(200).json(GAnswer);
   } catch (error) {
     return res.status(500).json({ error });
   }
 };
 
 
+//Update
+export const updateGQuestion = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { g_answer } = req.body;
+  try {
+    const GQuestions = await feature12Client.general_answers.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        g_answer: g_answer,
+      },
+    });
+    return res.status(200).json(GQuestions);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+
+/*
+//Delete
+export const deleteGQuestion = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const GQuestions = await feature12Client.general_questions.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+
+*/
--- src/routes/feature12.routes.ts
+++ src/routes/feature12.routes.ts
@@ -2,15 +2,24 @@ import { Router } from "express";
 
 // here import your controllers(function)
 import {
-  createGQuestions,
+  createGAnswer,
   getAllGeneralQuestions,
+  updateGQuestion,
+  displayAllGQuestions,
+  displayAnswer,
 } from "../controllers/feature12.controller";
 
 const feature12Router = Router();
 
 // here define your routes
-feature12Router.post("/", createGQuestions);
-feature12Router.get("/", getAllGeneralQuestions);
+feature12Router.post("/create", createGAnswer);
+feature12Router.get("/read", getAllGeneralQuestions);
+feature12Router.put("/update/:id", updateGQuestion);
+//feature12Router.delete("/deleteGQuestion/:id", deleteGQuestion);
+
+//Displaying all Questions
+feature12Router.get("/printAllQuestions", displayAllGQuestions);
+feature12Router.get("/printAnswer/:id", displayAnswer);
 
 
 export default feature12Router;
