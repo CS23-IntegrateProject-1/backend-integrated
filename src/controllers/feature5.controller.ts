376d2b59382df1198435a6082de642c2b851d18f
Dxchmxww <120299511+Dxchmxww@users.noreply.github.com>
2023-11-12 20:36:04 +0700

    [EDIT] controller

--- src/controllers/feature5.controller.ts
+++ src/controllers/feature5.controller.ts
@@ -1,24 +1,94 @@
-import { PrismaClient } from "@prisma/client";
+import { Customer_type, Prisma, PrismaClient, Target_group } from "@prisma/client";
 import { Response, Request } from "express";
+import { createdAdBusiness } from "../services/testService";
+// const bcrypt = require('bcrypt')
 
 const feature5Client = new PrismaClient();
 
 export const getfeature5 = async (req: Request, res: Response) => {
     res.status(200).json({message: 'This is Feature5'});
-
 };
 
-export const getAdvertisement = async (req: Request, res: Response) => {  
+type adinfo = {
+    name: string;
+    description: string;
+    image_url: string;
+    start_date: Date;
+    end_date: Date;
+    cost: number;
+    customer_type: Customer_type;
+    target_group: Target_group;
+    businessId: number;
+}
+
+export const AdBusiness = async (req: Request, res: Response) => {
+    console.log("first")
     try {
-        console.log("Hello world!");
-        const allAdvertisements = await feature5Client.advertisement.findMany();
-        res.status(200).json(allAdvertisements);
-            
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({ error: 'Failed to retrieve data' });
+        const {name,
+            description,
+            image_url,
+            start_date,
+            end_date,
+            cost,
+            customer_type,
+            target_group,
+        } = req.body;
+
+        const {id} = req.params;
+        const businessId = parseInt(id);
+        const adParams: adinfo = {
+            name : name,
+            description : description,
+            image_url : image_url,
+            start_date : start_date,
+            end_date : end_date,
+            cost : cost,
+            customer_type : customer_type,
+            target_group : target_group,
+            businessId : businessId
+        }
+        
+        const ad = await createdAdBusiness(adParams);
+        if (!ad){
+            res.status(404).json({ error: "not success"})
+        }
+        res.json({ message: 'add successfully', ad});
+
+    } catch(err) {
+        const error = err as Error;
+        res.status(500).json({ error: error.message});
     }
+    
+
 };
+
+export const AdminApprove = async (req: Request, res: Response) => {
+    try {
+        const {id} = req.params;
+        const { isApprove } = req.body;
+        const ApproveAd = await feature5Client.ad_business.update({
+            where: {advertisementId: parseInt(id)},
+            data: {isApprove}
+        })
+        res.json(ApproveAd)
+    } catch (err) {
+        const error = err as Error;
+        res.status(500).json({ error: error.message});
+    }
+}
+
+
+   // try {
+    //     console.log("Hello world!");
+    //     const allAdvertisements = await feature5Client.ad_business.findMany();
+    //     res.status(200).json(allAdvertisements);
+            
+    // } catch (e) {
+    //     console.log(e);
+    //     res.status(500).json({ error: 'Failed to retrieve data' });
+    // } 
+
+
 
 export const getUser = async (req: Request, res: Response) => {  
     try {
@@ -31,6 +101,7 @@ export const getUser = async (req: Request, res: Response) => {
         res.status(500).json({ error: 'Failed to retrieve data' });
     }
 };
+
 // example of controller getAllAuthors
 // export const getAllAuthors = async (req: Request, res: Response) => {
 //   try {
@@ -39,3 +110,4 @@ export const getUser = async (req: Request, res: Response) => {
 //     console.log(e);
 //   }
 // };
+
--- src/routes/feature5.routes.ts
+++ src/routes/feature5.routes.ts
@@ -2,16 +2,18 @@ import { Router } from "express";
 
 // here import your controllers(function)
 import {
+    AdBusiness,
     getfeature5,
-    getAdvertisement,
-    getUser
+    AdminApprove
+    //getUser
 } from "../controllers/feature5.controller";
 
 const feature5Router = Router();
 
 // here define your routes
 feature5Router.get("/", getfeature5);
-feature5Router.get("/Advertisement", getAdvertisement);
-feature5Router.get("/User", getUser);
+feature5Router.post("/AdBSN/:id", AdBusiness);
+feature5Router.patch("/AdminApprove/:id", AdminApprove);
+// feature5Router.get("/User", getUser);
 
 export default feature5Router;
