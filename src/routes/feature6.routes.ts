7905dd60cd36b5b9b89939802c82fe8dc5511562
Yunthecloud <sulei.yin1@kmutt.ac.th>
2023-11-12 23:44:57 +0700

    Revert "Merge pull request #22 from CS23-IntegrateProject-1/alpha"
    This reverts commit 1f1ee185339d14b5ae21cdddd8c20520e42db85a, reversing
    changes made to e47b7c04206e3bf35dcb0a9ee6d09b133c6847f0.

--- src/routes/feature6.routes.ts
+++ src/routes/feature6.routes.ts
@@ -1,28 +1,13 @@
 import { Router } from "express";
-import { getAllTable } from "../controllers/feature6.controller";
-import { getAllReservation } from "../controllers/feature6.controller";
-import { getAllReservationByStatus } from "../controllers/feature6.controller";
-import { getVenueById } from "../controllers/feature6.controller";
-import { getReservationById } from "../controllers/feature6.controller";
-// import { createReservation } from "../controllers/feature6.controller";
+
 // here import your controllers(function)
-// import {
-//     getfeature6,
-// } from "../controllers/feature6.controller";
+import {
+    getfeature6,
+} from "../controllers/feature6.controller";
 
 const feature6Router = Router();
 
 // here define your routes
-// feature6Router.get("/", getfeature6);
+feature6Router.get("/", getfeature6);
 
-//GET METHOD
-feature6Router.get("/allTable", getAllTable);
-feature6Router.get("/allReservation", getAllReservation);
-feature6Router.get("/MyReservation", getAllReservationByStatus);
-feature6Router.get("/venue/:venueId", getVenueById);
-feature6Router.get("/MyReservation/:reservationId", getReservationById);
-
-//POST METHOD
-// feature6Router.post("/createReservation", createReservation);
-// feature6Router.post("/createTable", createTable);
 export default feature6Router;
