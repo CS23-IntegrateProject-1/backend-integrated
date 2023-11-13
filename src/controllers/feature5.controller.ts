c0be131741f398781837b372fd054a3d81aafbb9
Dxchmxww <120299511+Dxchmxww@users.noreply.github.com>
2023-11-13 11:55:36 +0700

    [EDIT] controller

--- src/controllers/feature5.controller.ts
+++ src/controllers/feature5.controller.ts
@@ -1,6 +1,7 @@
 import { Customer_type, Prisma, PrismaClient, Target_group } from "@prisma/client";
 import { Response, Request } from "express";
-import { createdAdBusiness } from "../services/testService";
+import internal from "stream";
+// import { createdAdBusiness } from "../services/testService";
 // const bcrypt = require('bcrypt')
 
 const feature5Client = new PrismaClient();
@@ -9,7 +10,7 @@ export const getfeature5 = async (req: Request, res: Response) => {
     res.status(200).json({message: 'This is Feature5'});
 };
 
-type adinfo = {
+interface adinfo {
     name: string;
     description: string;
     image_url: string;
@@ -21,9 +22,22 @@ type adinfo = {
     businessId: number;
 }
 
+interface voucherinfo{
+    voucher_name: string;
+    voucher_image: string;
+    start_date: Date;
+    end_date: Date;
+    description: string;
+    point_use: number;
+    venueId: number;
+}
+
 export const AdBusiness = async (req: Request, res: Response) => {
-    console.log("first")
     try {
+        
+        const Tags: number[] = req.body.Tags;
+
+        const newAd: adinfo = req.body;
         const {name,
             description,
             image_url,
@@ -32,35 +46,80 @@ export const AdBusiness = async (req: Request, res: Response) => {
             cost,
             customer_type,
             target_group,
-        } = req.body;
+        } = newAd;
 
         const {id} = req.params;
-        const businessId = parseInt(id);
-        const adParams: adinfo = {
-            name : name,
-            description : description,
-            image_url : image_url,
-            start_date : start_date,
-            end_date : end_date,
-            cost : cost,
-            customer_type : customer_type,
-            target_group : target_group,
-            businessId : businessId
-        }
-        
-        const ad = await createdAdBusiness(adParams);
-        if (!ad){
-            res.status(404).json({ error: "not success"})
-        }
-        res.json({ message: 'add successfully', ad});
 
+        const newAdvertisement = await feature5Client.ad_business.create({
+            data: {
+                name,
+                description,
+                image_url,
+                start_date,
+                end_date,
+                cost,
+                customer_type,
+                target_group,
+                businessId: parseInt(id)
+            }
+        })
+
+        for(const tag of Tags){
+            await feature5Client.ad_tag.create({
+                data:{
+                    adId: newAdvertisement.advertisementId,
+                    tagId: tag
+                }
+            })
+        }
+
+        res.json(newAdvertisement);
     } catch(err) {
         const error = err as Error;
         res.status(500).json({ error: error.message});
     }
+};
+
+// export const AdBusiness = async (req: Request, res: Response) => {
+//     console.log("first")
+//     try {
+//         const {name,
+//             description,
+//             image_url,
+//             start_date,
+//             end_date,
+//             cost,
+//             customer_type,
+//             target_group,
+//         } = req.body;
+
+//         const {id} = req.params;
+//         const businessId = parseInt(id);
+//         const adParams: adinfo = {
+//             name : name,
+//             description : description,
+//             image_url : image_url,
+//             start_date : start_date,
+//             end_date : end_date,
+//             cost : cost,
+//             customer_type : customer_type,
+//             target_group : target_group,
+//             businessId : businessId
+//         }
+        
+//         const ad = await createdAdBusiness(adParams);
+//         if (!ad){
+//             res.status(404).json({ error: "not success"})
+//         }
+//         res.json({ message: 'add successfully', ad});
+
+//     } catch(err) {
+//         const error = err as Error;
+//         res.status(500).json({ error: error.message});
+//     }
     
 
-};
+// };
 
 export const AdminApprove = async (req: Request, res: Response) => {
     try {
@@ -77,30 +136,173 @@ export const AdminApprove = async (req: Request, res: Response) => {
     }
 }
 
-
-   // try {
-    //     console.log("Hello world!");
-    //     const allAdvertisements = await feature5Client.ad_business.findMany();
-    //     res.status(200).json(allAdvertisements);
-            
-    // } catch (e) {
-    //     console.log(e);
-    //     res.status(500).json({ error: 'Failed to retrieve data' });
-    // } 
-
-
-
-export const getUser = async (req: Request, res: Response) => {  
+export const DeleteAdvertisement = async (req: Request, res: Response) => {
     try {
-        // console.log("Hello world!");
-        const allUsers = await feature5Client.user.findMany();
-        res.status(200).json(allUsers);
+        const {id} = req.params;
+
+        const DeleteAd = await feature5Client.ad_business.delete({
+            where: {advertisementId: parseInt(id)},
             
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({ error: 'Failed to retrieve data' });
+        })
+        res.json(DeleteAd)
+    } catch (err) {
+        const error = err as Error;
+        res.status(500).json({ error: error.message});
     }
-};
+}
+
+export const GetAllAdvertisement = async (req: Request, res: Response) => {
+    try {
+        const GetallAd = await feature5Client.ad_business.findMany({
+            where: {isApprove: false},
+            
+        })
+        res.json(GetallAd)
+    } catch (err) {
+        const error = err as Error;
+        res.status(500).json({ error: error.message});
+    }
+}
+
+export const GetAllTags = async (req: Request, res: Response) => {
+    try {
+        const GetallAd = await feature5Client.a_tag.findMany();
+        res.json(GetallAd)
+    } catch (err) {
+        const error = err as Error;
+        res.status(500).json({ error: error.message});
+    }
+}
+
+export const Voucher = async (req: Request, res: Response) => {
+    try {
+        const newVch: voucherinfo = req.body;
+
+        const {
+            voucher_name,
+            voucher_image,
+            start_date,
+            end_date,
+            description,
+            point_use
+        } = newVch;
+
+        const {Vouchertype,id} = req.body;
+
+        const newVoucher = await feature5Client.voucher.create({
+            data:{
+                voucher_name,
+                voucher_image,
+                start_date,
+                end_date,
+                description,
+                point_use,
+                venueId: parseInt(id)
+            }
+        })
+
+        if(Vouchertype == "discount"){
+            const {
+                fix_discount,
+                percent_discount,
+                limitation,
+                minimum_spend
+            } = req.body;
+
+            await feature5Client.discount_voucher.create({
+                data:{
+                    fix_discount,
+                    percent_discount,
+                    limitation,
+                    minimum_spend,
+                    voucherId: newVoucher.voucherId
+                }
+            })
+        }
+
+        if(Vouchertype == "food"){
+            const {
+                limitation,
+                minimum_spend
+            } = req.body;
+
+            await feature5Client.food_voucher.create({
+                data:{
+                    limitation,
+                    minimum_spend,
+                    voucherId: newVoucher.voucherId
+                }
+            })
+        }
+
+        res.json(newVoucher)
+        
+    } catch (err) {
+        // const error = err as Error;
+        // res.status(500).json({ error: error.message});
+        const error = err as Error & { code?: string };
+        console.error('Prisma error:', error);
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const DeleteVoucher = async (req: Request, res: Response) => {
+    try {
+        const {id} = req.params;
+
+        const DeleteAd = await feature5Client.voucher.delete({
+            where: {voucherId: parseInt(id)},
+            
+        })
+        res.json(DeleteAd)
+    } catch (err) {
+        const error = err as Error;
+        res.status(500).json({ error: error.message});
+    }
+}
+
+export const VoucherApprove = async (req: Request, res: Response) => {
+    try {
+        const {id} = req.params;
+        const { isApprove } = req.body;
+        const ApproveVoucher = await feature5Client.voucher.update({
+            where: {voucherId: parseInt(id)},
+            data: {isApprove}
+        })
+        res.json(ApproveVoucher)
+    } catch (err) {
+        const error = err as Error;
+        res.status(500).json({ error: error.message});
+    }
+}
+
+export const GetallVenue = async (req: Request, res: Response) =>{
+    const{id} = req.params;
+    try {
+        const getvenue = await feature5Client.property.findMany({
+            where: {businessId: parseInt(id)},
+            include: {venue: true}
+        })
+        res.json(getvenue)
+
+    } catch (err) {
+        const error = err as Error;
+        res.status(500).json({ error: error.message});
+    }
+}
+
+
+// export const getUser = async (req: Request, res: Response) => {  
+//     try {
+//         // console.log("Hello world!");
+//         const allUsers = await feature5Client.user.findMany();
+//         res.status(200).json(allUsers);
+            
+//     } catch (e) {
+//         console.log(e);
+//         res.status(500).json({ error: 'Failed to retrieve data' });
+//     }
+// };
 
 // example of controller getAllAuthors
 // export const getAllAuthors = async (req: Request, res: Response) => {
--- src/routes/feature5.routes.ts
+++ src/routes/feature5.routes.ts
@@ -4,7 +4,14 @@ import { Router } from "express";
 import {
     AdBusiness,
     getfeature5,
-    AdminApprove
+    AdminApprove,
+    DeleteAdvertisement,
+    GetAllAdvertisement,
+    GetAllTags,
+    Voucher,
+    GetallVenue,
+    DeleteVoucher,
+    VoucherApprove
     //getUser
 } from "../controllers/feature5.controller";
 
@@ -14,6 +21,13 @@ const feature5Router = Router();
 feature5Router.get("/", getfeature5);
 feature5Router.post("/AdBSN/:id", AdBusiness);
 feature5Router.patch("/AdminApprove/:id", AdminApprove);
+feature5Router.delete("/DeleteAdBSN/:id", DeleteAdvertisement) ;
+feature5Router.get("/AllAdBSN", GetAllAdvertisement);
+feature5Router.get("/AllTag", GetAllTags);
+feature5Router.post("/Voucher", Voucher);
+feature5Router.get("/AllVenue/:id", GetallVenue);
+feature5Router.delete("/DeleteVoucher/:id", DeleteVoucher) ;
+feature5Router.patch("/VoucherApprove/:id", VoucherApprove);
 // feature5Router.get("/User", getUser);
 
 export default feature5Router;
