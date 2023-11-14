84614770e08b7b876e6605421c50ebc69995b605
Navin <ITdansaikul@gmail.com>
2023-11-14 14:54:24 +0700

    a

--- src/controllers/feature10.controller.ts
+++ src/controllers/feature10.controller.ts
@@ -8,16 +8,16 @@ export const getfeature10 = async (req: Request, res: Response) => {
     
 };
 
-// export const getAllFilms = async (req: Request, res: Response) => {
-//     try {
-//         const allFilms = await prisma.films.findMany();
-//         res.json(allFilms);
+export const getAllFilms = async (req: Request, res: Response) => {
+    try {
+        const allFilms = await prisma.films.findMany();
+        res.json(allFilms);
 
-//     } catch (err) {
-//         const error = err as Error;
-//         res.status(500).json({error: error.message});
-//     }
-// };
+    } catch (err) {
+        const error = err as Error;
+        res.status(500).json({error: error.message});
+    }
+};
 
 // export const getFilmsById = async (req: Request, res: Response) => {
 //     try {
@@ -155,7 +155,7 @@ export const getShowingFilms = async (req: Request, res: Response) => {
                 data = await filmsService.getShowing3DFilms();
             } else if (params.type === "X4D") {
                 data = await filmsService.getShowing4DFilms();
-            } else {
+            }  else {
                 data = await filmsService.getShowingKidFilms();
             }
         } else {
@@ -164,19 +164,54 @@ export const getShowingFilms = async (req: Request, res: Response) => {
         res.status(200).send(data);
     }
 
-export const getFilmsById = async (req: Request, res: Response) => {
-    try {
-        const id = Number(req.params.id);
-        const film = await prisma.films.findUnique({
-            where: {filmId: id}
-        });
-        res.json(film);
+// export const getFilmsById = async (req: Request, res: Response) => {
+//     try {
+//         const id = Number(req.params.id);
+//         const film = await prisma.films.findUnique({
+//             where: {filmId: id}
+//         });
+//         res.json(film);
 
-    } catch (err) {
-        const error = err as Error;
-        res.status(500).json({error: error.message});
+//     } catch (err) {
+//         const error = err as Error;
+//         res.status(500).json({error: error.message});
+//     }
+// }
+
+export const getFilmsById = async (req: Request, res: Response) => {
+  try {
+    const idParam = req.params.id;
+
+    //console.log('idParam:', idParam);
+
+    // Check if idParam is a valid number
+    const filmId = parseInt(idParam, 10);
+
+    //console.log('filmId:', filmId);
+
+    if (isNaN(filmId)) {
+      return res.status(400).json({ error: "Invalid film ID" });
     }
-}
+
+    const film = await prisma.films.findUnique({
+      where: {
+        filmId: filmId,
+      },
+    });
+
+    console.log('Film:', film);
+
+    if (!film) {
+      return res.status(404).json({ error: "Film not found" });
+    }
+
+    // Handle the retrieved film data
+    res.status(200).json({ film });
+  } catch (error) {
+    console.error("Error fetching film:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
 export const getNowShowingFilms = async (req: Request, res: Response) => {
     try {
@@ -187,6 +222,9 @@ export const getNowShowingFilms = async (req: Request, res: Response) => {
                 }
             }
         });
+        if (nowShowingFilms.length === 0) {
+            res.status(404).json({error: "No films are currently showing"});
+        }
         console.log("first")
         res.json(nowShowingFilms);
 
@@ -221,3 +259,20 @@ export const getUpcomingFilms = async (req: Request, res: Response) => {
 //     console.log(e);
 //   }
 // };
+
+export const getShowsByFilmsId = async (req: Request, res: Response) => {
+    try {
+        const id = Number(req.params.id);
+        const shows = await prisma.shows.findMany({
+            where: {
+                filmId: id
+            },
+        });
+        res.json(shows);
+
+    } catch (err) {
+        const error = err as Error;
+        res.status(500).json({error: error.message});
+    }
+    
+};
--- src/routes/feature10.routes.ts
+++ src/routes/feature10.routes.ts
@@ -6,7 +6,9 @@ import {
     getNowShowingFilms,
     getUpcomingFilms,
     getShowingFilms,
-    getFilmsById
+    getFilmsById,
+    getAllFilms,
+    getShowsByFilmsId
 } from "../controllers/feature10.controller";
 
 const feature10Router = Router();
@@ -15,6 +17,8 @@ const feature10Router = Router();
 feature10Router.get("/getNowshowingFilms", getNowShowingFilms);
 feature10Router.get("/getUpcomingFilms", getUpcomingFilms);
 feature10Router.get("/getShowingFilms", getShowingFilms);
-feature10Router.get("/getFilmsById", getFilmsById);
+feature10Router.get("/getFilmsById/:id", getFilmsById);
+feature10Router.get("/getAllFilms", getAllFilms);
+feature10Router.get("/getShowsByFilmsId/:id", getShowsByFilmsId);
 
 export default feature10Router;
