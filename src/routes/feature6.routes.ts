8e8c17adc2fb7284078d91b5029855d38b0d1a86
Watsawadee <109060632+Watsawadee@users.noreply.github.com>
2023-11-15 15:29:50 +0700

    [EDIT] Get Method

--- src/routes/feature6.routes.ts
+++ src/routes/feature6.routes.ts
@@ -5,15 +5,7 @@ import { getAllReservationByStatus } from "../controllers/feature6.controller";
 import { getVenueById } from "../controllers/feature6.controller";
 import { getVenueAndReservationsById } from "../controllers/feature6.controller";
 import { getReservationById } from "../controllers/feature6.controller";
-// import { createReservation } from "../controllers/feature6.controller";
-import { checkAvailability } from "../controllers/feature6.controller";
-import { getAvailableTables } from "../controllers/feature6.controller";
-import { findSuitableTable } from "../controllers/feature6.controller";
-// import { createTable } from "../controllers/feature6.controller";
-// import { createTableType } from "../controllers/feature6.controller";
 
-//IMPORT FOR BUSINESS SIDE
-// import { getAllTableTypeByVenueId } from "../controllers/feature6.controller";
 const feature6Router = Router();
 
 //GET METHOD
@@ -26,19 +18,4 @@ feature6Router.get("/MyReservation/:reservationId", getReservationById);
 feature6Router.get("/MyReservation", getAllReservationByStatus);
 feature6Router.get("/MyReservation/:venueId/:reservationId", getVenueAndReservationsById);
 
-//Not Finished
-feature6Router.get("/checkAvailability", checkAvailability);
-feature6Router.get("/getAvailableTables", getAvailableTables);
-feature6Router.get("/findSuitableTable", findSuitableTable);
-
-
-//Business Side Part
-
-//GET METHOD
-// feature6Router.get("/getAllTableType/:venueId",getAllTableTypeByVenueId);
-
-//POST METHOD
-// feature6Router.post("/createReservation", createReservation);
-// feature6Router.post("/createTable", createTable);
-// feature6Router.post("/createTableType", createTableType)
 export default feature6Router;
