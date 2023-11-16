dbf9eaf0273931806c7324e83716400658074988
Chotiwet Wisitworanat <chotiwet.wisi@icloud.com>
2023-11-16 17:24:38 +0700

    Verify authToken

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import UserType from "../interface/User.interface";
 
 class AuthService {
@@ -23,10 +23,26 @@ class AuthService {
 		return token;
 	}
 
+	decodeToken(token: string): JwtPayload {
+		const secretKey = process.env.JWT_SECRET as string;
+		const decoded: JwtPayload = jwt.verify(token, secretKey) as JwtPayload;
+		return decoded;
+	}
+
 	getUserByUsername(username: string) {
 		const user = this.prisma.user.findUnique({
 			where: {
 				username: username
+			}
+		});
+		if (!user) throw new Error("User not found.");
+		return user;
+	}
+
+	getUserById(userId: number) {
+		const user = this.prisma.user.findUnique({
+			where: {
+				userId: userId
 			}
 		});
 		if (!user) throw new Error("User not found.");
