d28f5b260482c4275ec459c336755b4c264870c6
Dylan Mac Yves <macyvesdylan@gmail.com>
2023-11-17 00:15:17 +0700

    added requests

--- src/routes/feature3.routes.ts
+++ src/routes/feature3.routes.ts
@@ -2,23 +2,35 @@ import { Router } from "express";
 
 // here import your controllers(function)
 import {
-    getVenues,
-    getVenueReviews,
-    getVenueOverviews,
-    getVenuePhotos,
-    getSearchHistory,
     getAdvertisements,
+    getVenues,
+    getVenueBranch,
+    getVenueContacts,
+    getVenueReviews,
+    getVenueKeywords,
+    getVenuePhotos,
+    getOpeningDay,
+    getSearchHistory,
+    addVenueReview,
+    editVenueReview,
+    deleteVenueReview,
 } from "../controllers/feature3.controller";
 
 const feature3Router = Router();
 
-// here define your routes
-feature3Router.get("/", getVenues);
-feature3Router.get("/reviews/:id", getVenueReviews);
-feature3Router.get("/overviews/:id", getVenueOverviews);
-feature3Router.get("/photos/:id", getVenuePhotos);
-feature3Router.get("/advertisement", getAdvertisements);
+// // here define your routes
+feature3Router.get("/advertisements", getAdvertisements);
+feature3Router.get("/venues", getVenues);
+feature3Router.get("/venue-branch/:branchId", getVenueBranch);
+feature3Router.get("/venue-contacts/:venueId", getVenueContacts);
+feature3Router.get("/venue-reviews/:id", getVenueReviews);
+feature3Router.get("/venue-keywords/:venueId", getVenueKeywords);
+feature3Router.get("/venue-photos/:venueId", getVenuePhotos);
+feature3Router.get("/opening-day/:venueId/:openingDayId", getOpeningDay);
+feature3Router.get("/search-history/:user_id", getSearchHistory);
 
-feature3Router.get("/history/:user_id", getSearchHistory);
+feature3Router.post("/venue-reviews", addVenueReview);
+feature3Router.patch("/venue-reviews/:venueReviewId", editVenueReview);
+feature3Router.delete("/venue-reviews/:venueReviewId", deleteVenueReview);
 
 export default feature3Router;
