e503ee458078424c376a503201dac706053d214f
Andhika_RK <andhikarestu873@gmail.com>
2023-11-20 22:21:06 +0700

    [EDIT] fix

--- src/routes/feature14.routes.ts
+++ src/routes/feature14.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 // here import your controllers(function)
 import {
-    getfeature14,
+    getAdminUser,
     createAdminUser,
     updateAdminUser,
 } from "../controllers/feature14.controller";
@@ -10,7 +10,7 @@ import {
 const feature14Router = Router();
 
 // here define your routes
-feature14Router.get("/", getfeature14);
+feature14Router.get("/", getAdminUser);
 feature14Router.post("/",createAdminUser);
 feature14Router.put("/:id",updateAdminUser);
 export default feature14Router;
