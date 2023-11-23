8e1ae7776dfd1f0713b9b849d48ae251eadd9484
Chotiwet Wisitworanat <chotiwet.wisi@icloud.com>
2023-11-23 20:08:13 +0700

    [ADD] Business user services

--- src/interface/Auth/IAuthService.interface.ts
+++ src/interface/Auth/IAuthService.interface.ts
@@ -1,6 +1,7 @@
 import { JwtPayload } from "jsonwebtoken";
 import { SentUser } from "./User.interface";
 import { SentAdminUser } from "./AdminUser.interface";
+import { SentBusisnessUser } from "./BusinessUser.interface";
 
 export default interface IAuthService {
 	verifyPassword: (
@@ -9,11 +10,17 @@ export default interface IAuthService {
 	) => Promise<boolean>;
 	hashPassword: (password: string) => string;
 	generateToken: (data: any, userType: string) => string;
-	decodeToken: (token: string) => JwtPayload;
+	decodeToken: (token: string) => any;
+
 	getUserByUsername: (username: string) => Promise<any>;
 	getAdminUserByUsername: (username: string) => Promise<any>;
+	getBusinessUserByUsername: (username: string) => Promise<any>;
+
 	getUserById: (userId: number) => Promise<any> | null;
 	getAdminUserById: (userId: number) => Promise<any>;
+	getBusinessUserById: (userId: number) => Promise<any>;
+
 	createUser: (user: SentUser) => Promise<any>;
 	createAdminUser: (user: SentAdminUser) => Promise<any>;
+	createBusinessUser: (user: SentBusisnessUser) => Promise<any>;
 }
