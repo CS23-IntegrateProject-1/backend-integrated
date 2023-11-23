fbf8bbc4bbd0f7fd5a6b84db43839b386fe0e646
Soe Thandar Lwin <engineer.soethandarlwin@gmail.com>
2023-11-23 23:02:46 +0700

    Remove hardcoded userId and implment proper decoding and error handling

--- src/controllers/feature1/ProfileController.ts
+++ src/controllers/feature1/ProfileController.ts
@@ -3,6 +3,9 @@ import { ProfileRepository } from "../../services/feature1/profile.repository";
 import ProfileService, {
   IProfileService,
 } from "../../services/feature1/profile.service";
+import { extractToken } from "./utils";
+import { makeErrorResponse } from "./models/payment_method.model";
+import jwt, { JsonWebTokenError } from "jsonwebtoken";
 
 export interface IProfileController {
   show(req: Request, res: Response): unknown;
@@ -14,8 +17,32 @@ export default class ProfileController implements IProfileController {
   );
 
   async show(req: Request, res: Response) {
-    const response = await this.service.getUserProfile(2);
+    let token: string;
 
-    return res.json(response);
+    try {
+      token = extractToken(req);
+    } catch (e) {
+      return res.status(401).json(makeErrorResponse("Unauthorized"));
+    }
+
+    try {
+      const decoded = jwt.verify(
+        token as string,
+        process.env.JWT_SECRET as string,
+      );
+      const userId = (decoded as jwt.JwtPayload).userId;
+
+      try {
+        const response = await this.service.getUserProfile(userId);
+
+        return res.json(response);
+      } catch (e) {
+        return res.status(404).json(makeErrorResponse("User not found"));
+      }
+    } catch (e) {
+      if (e instanceof JsonWebTokenError) {
+        return res.status(401).json(makeErrorResponse("Invalid token"));
+      }
+    }
   }
 }
