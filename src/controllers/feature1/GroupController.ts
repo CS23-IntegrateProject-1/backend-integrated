8fd98ad88ecc6df3c3f26d2cb33f000dfc536bcd
Soe Thandar Lwin <engineer.soethandarlwin@gmail.com>
2023-11-24 11:32:58 +0700

    Remove unused method

--- src/controllers/feature1/GroupController.ts
+++ src/controllers/feature1/GroupController.ts
@@ -7,16 +7,12 @@ import GroupService, {
 } from "../../services/feature1/group.service";
 import { extractToken } from "./utils";
 import { makeErrorResponse } from "./models/payment_method.model";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import {
   makeGroupCreateWebResponse,
-  makeGroupIndexWebResponse,
 } from "./models/group.model";
 import { PrismaClient } from "@prisma/client";
 
 export interface IGroupController {
-  addGroup(req: Request, res: Response): unknown;
-
   index(req: Request, res: Response): unknown;
 
   create(req: Request, res: Response): unknown;
@@ -97,51 +93,6 @@ export default class GroupController implements IGroupController {
     } catch (e) {
       if (e instanceof JsonWebTokenError) {
         return res.status(401).json(makeErrorResponse("Invalid token"));
-      }
-
-      return res
-        .status(500)
-        .json(makeErrorResponse("Unknown Error Encountered"));
-    }
-  }
-
-  async addGroup(req: Request, res: Response) {
-    let token: string;
-
-    try {
-      token = extractToken(req);
-    } catch (e) {
-      return res.status(401).json(makeErrorResponse("Unauthorized"));
-    }
-
-    const { group_id } = req.body;
-
-    try {
-      const decoded = jwt.verify(
-        token as string,
-        process.env.JWT_SECRET as string,
-      );
-      const userId = (decoded as jwt.JwtPayload).userId;
-
-      if (userId === group_id) {
-        return res
-          .status(409)
-          .json(makeErrorResponse("Cannot add oneself as group"));
-      }
-
-      await this.service.addGroupById(userId, group_id);
-
-      return res.status(200).send();
-    } catch (e) {
-      if (e instanceof JsonWebTokenError) {
-        return res.status(401).json(makeErrorResponse("Invalid token"));
-      } else if (e instanceof PrismaClientKnownRequestError) {
-        if (e.code === "P2002") {
-          return res
-            .status(409)
-            .json(makeErrorResponse("Already group with user"))
-            .send();
-        }
       }
 
       return res
