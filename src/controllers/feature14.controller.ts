13f34eefe30bf92418b6fea80fca304503cff245
IssadaornNK <49282901+IssadaornNK@users.noreply.github.com>
2023-12-17 18:39:29 +0700

    Update feature14.controller.ts

--- src/controllers/feature14.controller.ts
+++ src/controllers/feature14.controller.ts
@@ -3,95 +3,8 @@ import { Response, Request } from "express";
 import DashboardService from "../services/admin/dashboard.service";
 
 const feature14Client = new PrismaClient();
-<<<<<<< HEAD
-=======
 //=====================================ADMIN======================================
-export const createAdminUser = async (req: Request, res: Response) => {
-	const { username, hashed_password } = req.body;
-	try {
-		const admin_user = await feature14Client.admin_user.create({
-			data: {
-				username: username,
-				hashed_password: hashed_password,
-			},
-		});
-		return res.status(201).json({ admin_user });
-	} catch (error) {
-		return res.sendStatus(500).json({ error });
-	}
-};
 
-export const getAdminUser = async (req: Request, res: Response) => {
-	try {
-		const admin_user = await feature14Client.admin_user.findMany();
-		return res.status(200).json({ admin_user });
-	} catch (error) {
-		return res.status(500).json({ error });
-	}
-};
-
-export const updateAdminUser = async (req: Request, res: Response) => {
-	const { adminId } = req.params;
-	const { username, hashed_password } = req.body;
-	try {
-		const admin_user = await feature14Client.admin_user.update({
-			where: {
-				adminId: parseInt(adminId),
-			},
-			data: {
-				username: username,
-				hashed_password: hashed_password,
-			},
-		});
-		return res.status(201).json({ admin_user });
-	} catch (error) {
-		return res.sendStatus(500).json({ error });
-	}
-};
-//===============================BusinessUser==============================
-// export const createBusinessUser = async (req: Request, res: Response) => {
-// 	const { username, hashed_password } = req.body;
-// 	try {
-// 		const business_user = await feature14Client.business_user.create({
-// 			data: {
-// 				username: username,
-// 				hashed_password: hashed_password,
-// 			},
-// 		});
-// 		return res.status(201).json({ business_user });
-// 	} catch (error) {
-// 		return res.sendStatus(500).json({ error });
-// 	}
-// };
-
-// export const getBusinessUser = async (req: Request, res: Response) => {
-// 	try {
-// 		const business_user = await feature14Client.business_user.findMany();
-// 		return res.status(200).json({ business_user });
-// 	} catch (error) {
-// 		return res.status(500).json({ error });
-// 	}
-// };
-
-// export const updateBusinessUser = async (req: Request, res: Response) => {
-// 	const { businessId } = req.params;
-// 	const { username, hashed_password } = req.body;
-// 	try {
-// 		const business_user = await feature14Client.business_user.update({
-// 			where: {
-// 				businessId: parseInt(businessId),
-// 			},
-// 			data: {
-// 				username: username,
-// 				hashed_password: hashed_password,
-// 			},
-// 		});
-// 		return res.status(201).json({ business_user });
-// 	} catch (error) {
-// 		return res.sendStatus(500).json({ error });
-// 	}
-// };
->>>>>>> origin/Beta
 //===============================Dashboard==============================
 export const getDashboardChart = async (req: Request, res: Response) => {
 	try {
@@ -112,7 +25,6 @@ export const getDashboardChart = async (req: Request, res: Response) => {
 	}
 };
 
-<<<<<<< HEAD
 //===============================Report Ticket==============================
 export const createReportTicket =async (req:Request, res: Response) => {
 	const { title , Status, description, bussinessId } = req.body;
@@ -261,6 +173,3 @@ export const createHelpDesk = async( req:Request, res:Response) => {
 // };
 
 
-=======
-//===============================Dashboard==============================
->>>>>>> origin/Beta
