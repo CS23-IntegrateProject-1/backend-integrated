b72a452015c792c416b9c54e341cb57aacea3df1
IssadaornNK <49282901+IssadaornNK@users.noreply.github.com>
2023-12-17 14:44:24 +0700

    .

--- src/routes/feature14.routes.ts
+++ src/routes/feature14.routes.ts
@@ -2,22 +2,23 @@ import { Router } from "express";
 
 // here import your controllers(function)
 import {
-	getAdminUser,
-	createAdminUser,
-	updateAdminUser,
+	// getAdminUser,
+	// createAdminUser,
+	// updateAdminUser,
 	// createBusinessUser,
 	// getBusinessUser,
 	// updateBusinessUser,
 	getDashboardChart,
+	ticketResponse,
 } from "../controllers/feature14.controller";
 
 const feature14Router = Router();
 
 // here define your routes
 feature14Router.get("/getDashboard", getDashboardChart);
-feature14Router.get("/getAdmin", getAdminUser);
-feature14Router.post("/Admin", createAdminUser);
-feature14Router.put("/Admin/:id", updateAdminUser);
+// feature14Router.get("/getAdmin", getAdminUser);
+// feature14Router.post("/Admin", createAdminUser);
+// feature14Router.put("/Admin/:id", updateAdminUser);
 // feature14Router.post("/", createBusinessUser);
 // feature14Router.get("/", getBusinessUser);
 // feature14Router.put("/:id", updateBusinessUser);
