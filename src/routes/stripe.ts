35b1459ef181e3f33fb2219f92ff5a0682dc1cb5
tartartar <112954170+itsChewin@users.noreply.github.com>
2023-12-17 16:12:43 +0700

    [EDIT] Stripe
    Edit file

--- src/routes/stripe.ts
+++ src/routes/stripe.ts
@@ -1,36 +1,39 @@
-const express = require("express");
+// server.js
+
+const express = require('express');
 const app = express();
-const stripe = require("stripe")("sk_test_51OFf98BCLtNTpQNyJKiknfWUhqRZoF6SJa6zymmaQAVCtwnSoydmxZ6PzYg5OkyniCJe9GIrlxB8J1l0sYeqCEY400mOZap4J5"); // Replace with your actual Stripe secret key
+const stripe = require('stripe')(process.env.STRIP_KEY);
 
 app.use(express.json());
 
-app.post("/create-checkout-session", async (req, res) => {
-  try {
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
+app.post('/create-checkout-session', async (req, res) => {
+    try {
+        
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
       line_items: [
         {
           price_data: {
-            currency: "usd",
+            currency: 'usd',
             product_data: {
-              name: "Stubborn Attachments",
-              images: ["https://i.imgur.com/EHyR2nP.png"],
+              name: 'Stubborn Attachments',
+              images: ['https://i.imgur.com/EHyR2nP.png'],
             },
             unit_amount: 2000,
           },
           quantity: 1,
         },
       ],
-      mode: "payment",
-      success_url: "http://localhost:3000/success?success=true",
-      cancel_url: "http://localhost:3000/cancel?canceled=true",
-    });
+      mode: 'payment',
+      success_url: 'http://localhost:3000/success?success=true',
+      cancel_url: 'http://localhost:3000/cancel?canceled=true',
+        });
 
-    res.json({ id: session.id });
-} catch (error: any) {
-    console.error("Error creating checkout session:", error.message);
-    res.status(500).send("Internal Server Error");
-}
+        res.json({ id: session.id });
+    } catch (error: any) {
+        console.error('Error creating checkout session:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 const PORT = 4000;
