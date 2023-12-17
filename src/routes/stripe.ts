f0186dcf91230c301333abc66a35092b75d0b11a
tartartar <112954170+itsChewin@users.noreply.github.com>
2023-12-17 16:04:34 +0700

    [EDIT] Stripe

--- src/routes/stripe.ts
+++ src/routes/stripe.ts
@@ -1,25 +1,39 @@
-// This is your test secret API key.
-const stripe = require('stripe')('sk_test_51OFf98BCLtNTpQNyJKiknfWUhqRZoF6SJa6zymmaQAVCtwnSoydmxZ6PzYg5OkyniCJe9GIrlxB8J1l0sYeqCEY400mOZap4J5');
-const express = require('express');
+const express = require("express");
 const app = express();
-app.use(express.static('public'));
+const stripe = require("stripe")("sk_test_51OFf98BCLtNTpQNyJKiknfWUhqRZoF6SJa6zymmaQAVCtwnSoydmxZ6PzYg5OkyniCJe9GIrlxB8J1l0sYeqCEY400mOZap4J5"); // Replace with your actual Stripe secret key
 
-const YOUR_DOMAIN = 'http://localhost:4000';
+app.use(express.json());
 
-app.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price: 'price_1OFtzuBCLtNTpQNy5d50ybcQ',
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: `${YOUR_DOMAIN}?success=true`,
-    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
-  });
+app.post("/create-checkout-session", async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: "Stubborn Attachments",
+              images: ["https://i.imgur.com/EHyR2nP.png"],
+            },
+            unit_amount: 2000,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      success_url: "http://localhost:3000/success?success=true",
+      cancel_url: "http://localhost:3000/cancel?canceled=true",
+    });
 
-  res.redirect(303, session.url);
+    res.json({ id: session.id });
+} catch (error: any) {
+    console.error("Error creating checkout session:", error.message);
+    res.status(500).send("Internal Server Error");
+}
 });
 
-app.listen(4000, () => console.log('Running on port 4000'));
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
