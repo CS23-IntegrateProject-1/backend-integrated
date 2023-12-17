b8f8b96faba27ad86784da05b90bff90f976c34d
tartartar <112954170+itsChewin@users.noreply.github.com>
2023-12-17 14:20:06 +0700

    [EDIT] Stripe

--- src/routes/stripe.ts
+++ src/routes/stripe.ts
@@ -1,25 +1,35 @@
-const stripe = require('stripe')('sk_test_51OFf98BCLtNTpQNyJKiknfWUhqRZoF6SJa6zymmaQAVCtwnSoydmxZ6PzYg5OkyniCJe9GIrlxB8J1l0sYeqCEY400mOZap4J5');
-const express = require('express');
+// Import necessary modules and types from Stripe
+import Stripe from 'stripe';
+import express, { Request, Response } from 'express';
+
+// Replace 'your-secret-key' with your actual secret API key from Stripe
+const stripe = new Stripe('sk_test_51OFf98BCLtNTpQNyJKiknfWUhqRZoF6SJa6zymmaQAVCtwnSoydmxZ6PzYg5OkyniCJe9GIrlxB8J1l0sYeqCEY400mOZap4J5');
+
 const app = express();
 app.use(express.static('public'));
 
 const YOUR_DOMAIN = 'http://localhost:4242';
 
-app.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: 'price_1OFtzuBCLtNTpQNy5d50ybcQ',
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: `${YOUR_DOMAIN}/success.html`,
-    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
-  });
+app.post('/create-checkout-session', async (req: Request, res: Response) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          price: 'price_1OFtzuBCLtNTpQNy5d50ybcQ',
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `${YOUR_DOMAIN}/success.html`,
+      cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+    });
 
-  res.redirect(303, session.url);
+    
+  } catch (error) {
+    console.error('Error creating checkout session:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 app.listen(4242, () => console.log('Running on port 4242'));
