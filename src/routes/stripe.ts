1e0d9d899c9ef0b6f7d2baa46fa032f3c3cfd51e
tartartar <112954170+itsChewin@users.noreply.github.com>
2023-12-17 15:58:15 +0700

    [EDIT] edit port

--- src/routes/stripe.ts
+++ src/routes/stripe.ts
@@ -4,13 +4,12 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-const YOUR_DOMAIN = 'http://localhost:4242';
+const YOUR_DOMAIN = 'http://localhost:4000';
 
 app.post('/create-checkout-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
-        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
         price: 'price_1OFtzuBCLtNTpQNy5d50ybcQ',
         quantity: 1,
       },
@@ -23,4 +22,4 @@ app.post('/create-checkout-session', async (req, res) => {
   res.redirect(303, session.url);
 });
 
-app.listen(4242, () => console.log('Running on port 4242'));
+app.listen(4242, () => console.log('Running on port 4000'));
