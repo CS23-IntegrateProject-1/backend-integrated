58fe2cf64babb27eec1f617ca820a6349e2f314b
Watsawadee <109060632+Watsawadee@users.noreply.github.com>
2023-12-19 03:20:51 +0700

    [UPDATE] API MIK Checkout

--- src/routes/MIKAPI.routes.ts
+++ src/routes/MIKAPI.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { ApiConfirmReserve, ApiConnection, ApiReserve } from "../controllers/MIKAPI.controller";
+import { ApiCheckOut, ApiConfirmReserve, ApiConnection, ApiReserve } from "../controllers/MIKAPI.controller";
 const MIKAPIRouter = Router();
 
 MIKAPIRouter.get("/test", ApiConnection);
 MIKAPIRouter.post("/reserve", ApiReserve);
 MIKAPIRouter.patch("/confirm", ApiConfirmReserve);
+MIKAPIRouter.patch("/checkout", ApiCheckOut);
 
 
 
