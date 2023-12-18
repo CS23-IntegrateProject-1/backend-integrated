e9fc633d632e0b64e785023611138315e5157db0
Watsawadee <109060632+Watsawadee@users.noreply.github.com>
2023-12-19 06:28:50 +0700

    Revert "Merge branch 'Beta' into feature6-new-beta"
    This reverts commit 759c8588a107023141d635fa339522c59537b181, reversing
    changes made to 209ffded1de2208006ff291fc1a05b94762d8a06.

--- prisma/migrations/20231218222524_add_relation_show/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE `Reservation_logs` ADD CONSTRAINT `Reservation_logs_showId_fkey` FOREIGN KEY (`showId`) REFERENCES `Shows`(`showId`) ON DELETE RESTRICT ON UPDATE CASCADE;
