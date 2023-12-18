ea0609d24ec1a2d39cd320167143fa6fb25331b4
woodyy <woodyy.nmk@gmail.com>
2023-12-19 01:47:27 +0700

    [fix] accidental unknown error on feature-3 routes

--- src/routes/feature3.routes.ts
+++ src/routes/feature3.routes.ts
@@ -41,36 +41,36 @@ import {
 const feature3Router = Router();
 
 // Routes
-// feature3Router.get("/advertisements", getAdvertisements);
-// feature3Router.get("/venues", getVenues);
-// feature3Router.get("/venues/:category", getVenuesByCategory);
-// feature3Router.get("/venue-branch/:branchId", getVenueBranch);
-// feature3Router.get("/menus/:venueId", getAllMenusByVenueId);
-// feature3Router.get("/venue-contacts/:venueId", getVenueContacts);
-// feature3Router.get("/recommended-venues", getRecommendedVenues);
-// feature3Router.get("/venue-ratings", getVenuesRatings);
-// feature3Router.get("/venue-keywords/:venueId", getVenueKeywords);
-// feature3Router.get("/venue-keywords", getPhotos);
-// feature3Router.get("/venue-photos/:venueId", getVenuePhotos);
-// feature3Router.get("/opening-day/:venueId/:openingDayId", getOpeningDay);
-// feature3Router.get("/search-history/:user_id", getSearchHistory);
-// feature3Router.get("/food-vouchers", getAllFoodVouchers);
-// feature3Router.get("/discount-vouchers", getAllDiscountVouchers);
+feature3Router.get("/advertisements", getAdvertisements);
+feature3Router.get("/venues", getVenues);
+feature3Router.get("/venues/:category", getVenuesByCategory);
+feature3Router.get("/venue-branch/:branchId", getVenueBranch);
+feature3Router.get("/menus/:venueId", getAllMenusByVenueId);
+feature3Router.get("/venue-contacts/:venueId", getVenueContacts);
+feature3Router.get("/recommended-venues", getRecommendedVenues);
+feature3Router.get("/venue-ratings", getVenuesRatings);
+feature3Router.get("/venue-keywords/:venueId", getVenueKeywords);
+feature3Router.get("/venue-keywords", getPhotos);
+feature3Router.get("/venue-photos/:venueId", getVenuePhotos);
+feature3Router.get("/opening-day/:venueId/:openingDayId", getOpeningDay);
+feature3Router.get("/search-history/:user_id", getSearchHistory);
+feature3Router.get("/food-vouchers", getAllFoodVouchers);
+feature3Router.get("/discount-vouchers", getAllDiscountVouchers);
 
-// // Sentiment analysis
-// feature3Router.get("/sentiment-analysis/:id", getSentimentAnalysis);
+// Sentiment analysis
+feature3Router.get("/sentiment-analysis/:id", getSentimentAnalysis);
 
-// // Venue reviews
-// feature3Router.get("/venue-reviews/:id", getVenueReviews);
-// feature3Router.post("/venue-reviews", addVenueReview);
-// feature3Router.patch("/venue-reviews/:venueReviewId", editVenueReview);
-// feature3Router.delete("/venue-reviews/:venueReviewId", deleteVenueReview);
+// Venue reviews
+feature3Router.get("/venue-reviews/:id", getVenueReviews);
+feature3Router.post("/venue-reviews", addVenueReview);
+feature3Router.patch("/venue-reviews/:venueReviewId", editVenueReview);
+feature3Router.delete("/venue-reviews/:venueReviewId", deleteVenueReview);
 
-// // Food reviews
-// feature3Router.get("/food-reviews/:id", getFoodReviews);
-// feature3Router.post("/food-reviews", addFoodReview);
-// feature3Router.patch("/food-reviews/:foodReviewId", editFoodReview);
-// feature3Router.delete("/food-reviews/:foodReviewId", deleteFoodReview);
+// Food reviews
+feature3Router.get("/food-reviews/:id", getFoodReviews);
+feature3Router.post("/food-reviews", addFoodReview);
+feature3Router.patch("/food-reviews/:foodReviewId", editFoodReview);
+feature3Router.delete("/food-reviews/:foodReviewId", deleteFoodReview);
 
 
 
