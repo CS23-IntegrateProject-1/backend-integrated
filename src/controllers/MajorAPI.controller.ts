3882a5ca3c2a079222b0fd67a8454b6daf81abb7
Navin <ITdansaikul@gmail.com>
2023-12-19 02:17:51 +0700

    [ADD]apitheater

--- src/controllers/MajorAPI.controller.ts
+++ src/controllers/MajorAPI.controller.ts
@@ -1,19 +1,14 @@
 import { Request, Response } from "express";
 import majorAPIService from "../services/movie/majorAPI.service";
 import filmsService from "../services/movie/films.service";
+import theatersService from "../services/movie/theaters.service";
 import { Film } from "../interface/movie/film.interface";
 import { Theater } from "../interface/movie/theater.interface";
 
 const getFilmFromHarmoni = filmsService.getAllFilms;
+const getTheaterFromHarmoni = theatersService.getAllTheaters;
 
 class MajorAPIController {
-  // async getFilmsFromMajor(req: Request, res: Response) {
-  //     try {
-  //         const response = await majorAPIService.getFilmsFromMajor();
-  //         res.status(200).send(response);
-  //        } catch(e: any) {}
-  // }
-
   async getFilmsFromMajor(req: Request, res: Response) {
     try {
       const majorData: Film[] = await majorAPIService.getFilmsFromMajor();
@@ -36,8 +31,21 @@ class MajorAPIController {
 
   async getTheaterFromMajor(req: Request, res: Response) {
     try {
-      const response = await majorAPIService.getTheaterFromMajor();
-      res.status(200).send(response);
+      const majorData: Theater[] = await majorAPIService.getTheatersFromMajor();
+      const harmoniData: Theater[] = await getTheaterFromHarmoni();
+
+      const newDataMajor: Theater[] = majorData.filter(
+        (theater) => !harmoniData.some((hTheater) => hTheater.theaterId === theater.theaterId)
+      );
+
+      const deletedDataMajor: Theater[] = harmoniData.filter(
+        (hTheater) => !majorData.some((theater) => theater.theaterId === hTheater.theaterId)
+      );
+
+      majorAPIService.addNewTheaters(newDataMajor);
+      majorAPIService.deleteOutdatedTheaters(deletedDataMajor);
+
+      res.status(200).send(majorData);
     } catch (e: any) {}
   }
 
---
+++ src/services/movie/majorAPI.service.ts
@@ -0,0 +1,112 @@
+import { Genre, PrismaClient } from "@prisma/client";
+import { Axios } from "../../configs/MajorAxiosInstance";
+import { Film } from "../../interface/movie/film.interface";
+import { Theater } from "../../interface/movie/theater.interface";
+import theatersService from "./theaters.service";
+
+class MajorAPIService {
+  prisma = new PrismaClient();
+  async getFilmsFromMajor() {
+    try {
+      const response = await Axios.get("/film/getFilm");
+      return response.data;
+    } catch (e: any) {
+      console.log(e);
+    }
+  }
+
+  async addNewFilms(addQueue: Film[]) {
+    try {
+      for (const film of addQueue) {
+        const genre = film.genre;
+        await this.prisma.films.create({
+          data: {
+            filmId: film.filmId,
+            name: film.name,
+            language: film.language,
+            synopsis: film.synopsis,
+            release_date: film.release_date,
+            duration: film.duration,
+            poster_img: film.poster_img,
+            rate: film.rate,
+            genre: Genre[genre],
+          },
+        });
+      }
+    } catch (e: any) {
+      console.log(e);
+    }
+  }
+
+  async deleteOutdatedFilms(deleteQueue: Film[]) {
+    try {
+      for (const film of deleteQueue) {
+        await this.prisma.films.delete({
+          where: {
+            filmId: film.filmId,
+          },
+        });
+      }
+    } catch (e: any) {
+      console.log(e);
+    }
+  }
+
+  async getTheatersFromMajor() {
+    try {
+      const response = await Axios.get("/theater/getTheaters");
+      return response.data;
+    } catch (e: any) {
+      console.log(e);
+    }
+  }
+
+  async addNewTheaters(addQueue: Theater[]) {
+    try {
+      for (const theater of addQueue) {
+        await this.prisma.theaters.create({
+          data: {
+            theaterId: theater.theaterId,
+            name: theater.name,
+            address: theater.address,
+            phone_num: theater.phone_num,
+            promptpay_num: theater.promptpay_num,
+            latitude: theater.latitude,
+            longitude: theater.longitude,
+          },
+        });
+      }
+    } catch (e: any) {
+      console.log(e);
+    }
+  }
+
+  async deleteOutdatedTheaters(deleteQueue: Theater[]) {
+    try {
+      for (const theater of deleteQueue) {
+        await this.prisma.theaters.delete({
+          where: {
+            theaterId: theater.theaterId,
+          },
+        });
+      }
+    } catch (e: any) {
+      console.log(e);
+    }
+  }
+
+  async getSeatTypeFromMajor() {
+    try {
+      const id = 1;
+      const showId = 3;
+      const response = await Axios.get(
+        `/seat/getUniqueSeatTypeByScreenId/${id}/${showId}`
+      );
+      return response.data;
+    } catch (e: any) {
+      console.log(e);
+    }
+  }
+}
+
+export default new MajorAPIService();
