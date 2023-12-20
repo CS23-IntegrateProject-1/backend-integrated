5d35287db67bc4fd59abb9c92a4982f697fd951f
IssadaornNK <49282901+IssadaornNK@users.noreply.github.com>
2023-12-20 16:15:21 +0700

    service

--- src/services/admin/dashboard.service.ts
+++ src/services/admin/dashboard.service.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { th } from "date-fns/locale";
 
 class DashboardService {
 	prisma = new PrismaClient();
@@ -102,6 +103,40 @@ class DashboardService {
 		} catch (e) {
 			console.log(e);
 			throw new Error("Error in getting number of reciept");
+		}
+	}
+	async getAllTransaction() {
+		try {
+			const AppTransaction = this.prisma.transaction_detail.groupBy({
+				by: ['total_amount'],
+				_sum: {
+					total_amount: true,
+				},
+				where: {
+					status: "Completed"
+				}
+			});
+			// const appTransactionResult = await AppTransaction;
+			// const firstTransaction = appTransactionResult[0];
+			// const AllTransactionCount = firstTransaction?._sum?.total_amount;
+			// return (await AllTransactionCount);
+			const appTransactionResult = await AppTransaction;
+			const totalAmountSum = appTransactionResult.reduce((sum, transaction) => {
+				const transactionAmount = transaction?._sum?.total_amount;
+				const amountToAdd = typeof transactionAmount === 'number'
+					? transactionAmount
+					: transactionAmount?.toNumber() ?? 0;
+				return sum + amountToAdd;
+			}, 0);
+
+			return {
+				Revenue: totalAmountSum,
+				Partner: totalAmountSum*90/100,
+				NetProfit: totalAmountSum*10/100,
+			}
+		} catch (e) {
+			console.log(e);
+			throw new Error("Error in getting number of transaction");
 		}
 	}
 }
