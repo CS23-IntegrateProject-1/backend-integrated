eeee25690a83f0a20f08672170b0f18038605c94
tartartar <112954170+itsChewin@users.noreply.github.com>
2023-12-20 18:23:26 +0700

    [EDIT] stripe controller page

--- src/controllers/feature8.controller.ts
+++ src/controllers/feature8.controller.ts
@@ -2093,8 +2093,8 @@ export const createDepositSession = async (req: Request, res: Response) => {
           },
         ],
         mode: "payment",
-        success_url: `${process.env.CLIENT_URL}/checkout-success`,
-        cancel_url: `${process.env.CLIENT_URL}/checkout-cancel`,
+        success_url: `${process.env.CLIENT_URL}/deposit-success`,
+        cancel_url: `${process.env.CLIENT_URL}/deposit-cancel`,
       } as any);
 
       await feature8Client.reservation.update({
@@ -2175,8 +2175,8 @@ export const createSeatSessionnn = async (req: Request, res: Response) => {
         },
       ],
       mode: "payment",
-      success_url: `${process.env.CLIENT_URL}/`,
-      cancel_url: `${process.env.CLIENT_URL}/`,
+      success_url: `${process.env.CLIENT_URL}/seat-success`,
+      cancel_url: `${process.env.CLIENT_URL}/seat-cancel`,
     });
 
     res.status(200).json({ url: session.url });
