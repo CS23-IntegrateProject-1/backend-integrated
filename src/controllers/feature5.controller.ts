9ddbdaa406e87d3152eca9f886685c6a8c615bef
Dxchmxww <120299511+Dxchmxww@users.noreply.github.com>
2023-12-22 13:27:42 +0700

    [EDIT] delette

--- src/controllers/feature5.controller.ts
+++ src/controllers/feature5.controller.ts
@@ -1281,22 +1281,6 @@ export const GetRedeembyId = async (req: Request, res: Response) => {
   }
 };
 
-export const GetDeleteRedeembyId = async (req: Request, res: Response) => {
-	try {
-		const { redeemId } = req.params;
-		const GetDeletebyId = await feature5Client.redeem_privilege.delete({
-			where: {
-				redeemId: parseInt(redeemId),
-			},
-		});
-
-		res.json(GetDeletebyId);
-	} catch (err) {
-		const error = err as Error;
-    console.log(err)
-		res.status(500).json({ error: error.message });
-	}
-};
 
 export const GetDeleteRedeembyId = async (req: Request, res: Response) => {
 	try {
