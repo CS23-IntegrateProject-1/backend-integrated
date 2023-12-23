118177555b7694e93c7e7d6fc7aeef80af63625f
Navin <ITdansaikul@gmail.com>
2023-12-24 04:55:01 +0700

    [edit]

--- src/services/movie/payment.service.ts
+++ src/services/movie/payment.service.ts
@@ -7,35 +7,40 @@ class paymentService {
   }
 
   async waitForPayment(harmoniLogs: number[]) {
-    const prisma = new PrismaClient();
-    await this.delay(0.5 * 60 * 1000);
-    for (const reservationId of harmoniLogs) {
-      const response: any[] = await prisma.payments.findMany({
-        where: {
-          reservationId: reservationId,
-        },
-      });
-      if (response[0].payment_status != "Success") {
-        await prisma.payments.delete({
+    try {
+      const prisma = new PrismaClient();
+      await this.delay(0.5 * 60 * 1000);
+      for (const reservationId of harmoniLogs) {
+        const response: any[] = await prisma.payments.findMany({
           where: {
             reservationId: reservationId,
           },
         });
-        const harmoniResponse = await prisma.reservation_logs.delete({
-          where: {
-            reservationId: reservationId,
-          },
-        });
-        console.log("deleted: " + reservationId);
+        if (response[0].payment_status != "Success") {
+          await prisma.payments.delete({
+            where: {
+              reservationId: reservationId,
+            },
+          });
+          const harmoniResponse = await prisma.reservation_logs.delete({
+            where: {
+              reservationId: reservationId,
+            },
+          });
+          console.log("deleted: " + reservationId);
 
-        const showId = harmoniResponse.showId;
-        const seatId = harmoniResponse.seatId;
-        MajorAxios.post("api/reservaion/delete", {
-          showId,
-          seatId,
-        });
+          const showId = harmoniResponse.showId;
+          const seatId = harmoniResponse.seatId;
+          MajorAxios.post("api/reservaion/delete", {
+            showId,
+            seatId,
+          });
+        }
       }
+    } catch (e: any) {
+      console.log(e);
     }
+    
   }
 }
 
